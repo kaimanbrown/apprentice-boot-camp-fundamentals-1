3d683ac5f6e2672735bff941ec3c5a4ab37c7a63
Kaiman Brown <kaiman.brown@autotrader.co.uk>
2025-01-03 15:13:03 +0000

    WIP Replace int array with PlayingCard
    - [X] Change suit from int to String in PlayingCard
    - [X] Create PlayingCard.toString()
    - [ ] Change return type of getCards() to use the list

--- exercises/java/src/main/java/cards/PlayingCard.java
+++ exercises/java/src/main/java/cards/PlayingCard.java
@@ -1,6 +1,17 @@
 package cards;
 
 public class PlayingCard {
+
+    private final String suit;
+    private final String name;
+
     public PlayingCard(String suit, String name) {
+        this.suit = suit;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " of " + suit;
     }
 }
